e4e49b3bd8bf146a92adde366527ee2ac8931d12
Ranjith_Ittaboina <112500071+Ranjith-Ittaboina@users.noreply.github.com>
2022-10-29 18:36:51 +0530

    assignment 1.1 of python certification preparation

---
+++ assignment_1_1.py
@@ -0,0 +1,71 @@
+# Q1. Name few data types of python?
+# A1. int, float, string, bool, complex, list, tuple, set, dict, None
+
+# Q2. What is the difference between list and tuples in Python?
+# A2. List: mutable, slower than tuple
+#     Tuple: immutable, faster than list
+
+# Q3. How to find type of data?
+# A3. type() function is used to find the data type
+
+# Q4. What kind of error is type error?
+# A4. It is a run time error
+
+# Q5. how to remove duplicate items of list?  
+# A5. convert list to set and again to list.
+
+# Q6. which operator is used for increment?
+# A6. "+=", ++ operator is not defined in python
+
+# Q7. what does "%" operator returns?
+# A7. It returns remainder of division.
+
+# Q8. What is the output of the following code : print 9//2?
+# A8. 4
+
+# Q9. What is the output of the following code : print 9/2?
+# A9. 4.5
+
+# Q10. What is the output of the following code : print 2**3?
+# A10. 8
+
+# Q11. What is pass in Python?
+# A11. Pass means, no-operation Python statement, or in other words it is a place holder in compound statement, 
+#      where there should be a blank left and nothing has to be written there.
+
+# Q12. What Is The Difference Between Pass And Continue In Python? 
+# A12. The continue statement makes the loop to resume from the next iteration.
+#      On the contrary, the pass statement instructs to do nothing, and the remainder of the code executes as usual.
+
+# Q13. What Is A Built-In Function That Python Uses To Iterate Over A Number Sequence?
+# A13. Range() generates a list of numbers, which is used to iterate over for loops.
+
+# Q14. What is the output of : lst = [x for x in range(1,11)]
+# A14. lst = [1,2,3,4,5,6,7,8,9,10]
+
+# Q15. What is lambda in Python?
+# A15. anonymous function means that a function is without a name. As we already know that def keyword is used 
+#      to define the normal functions and the lambda keyword is used to create anonymous functions.
+
+# Q16. Mention the use of the split function in Python?
+# A16. The use of the split function in Python is that it breaks a string into shorter strings using the 
+#      defined separator. It gives a list of all words present in the string.
+
+# Q17. Is It Mandatory For A Python Function To Return A Value? 
+# A17. It is not at all necessary for a function to return any value. However, if needed, we can use None 
+#      as a return value.
+
+# Q18. What is *args and **kwargs?
+# A18. *args is used when the programmer is not sure about how many arguments are going to be passed to a 
+#      function, or if the programmer is expecting a list or a tuple as argument to the function.
+#     **kwargs is used when a dictionary (keyword arguments) is expected as an argument to the function.
+
+# Q19. What is a Counter in Python? 
+# A19. The function Counter() from the module ‘collections’. It counts the number of occurrences of the elements 
+#      of a container.
+
+# Q20. When do you choose a list over a tuple?
+# A20. When there is an immutable ordered list of elements we choose tuple. Because we cannot add/remove an 
+#      element from the tuple. On the other hand, we can add elements to a list using append () or extend() or insert(),
+#      etc., and delete elements from a list using remove() or pop(). 
+
